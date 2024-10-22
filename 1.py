f32d83d4cdaa2d41721d6b57f561e22d6c2f7d37
Kevin <77845700+KevinSajuThomas@users.noreply.github.com>
2024-10-22 16:07:30 +0530

    Update 1.py

--- 1.py
+++ 1.py
@@ -1,3 +1,7 @@
+#The person is booking the railway ticket based on their age and the number of tickets they want to purchase.
+#The prices are regular price : rs.50, senior citizen (Age 60 and above) price : rs.30 and for the children 
+#age 12 and below price is :rs.20.Find out total cost based on the their age and the number of tickets and display it.
+
 def calculate(age, tickets):
     if age >= 60:
         ticket_price = 30
